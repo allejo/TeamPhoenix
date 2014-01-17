117b4f210c076c7d6d7ee402fe2e128a33465dbf
allejo <allejo@me.com>
2014-01-16 21:52:22 -0800

    Cleanup of code and info files

---
+++ README.markdown
@@ -0,0 +1,72 @@
+Team Phoenix
+================
+
+Based on Phoenix plugin, this plugin will reset the positions for all teams not just one team.
+
+Authors
+------
+
+- Vladimir "allejo" Jimenez
+
+Compiling
+---------
+
+### Requirements
+
+- BZFlag 2.4.0
+
+### How to Compile
+
+1.  Check out the BZFlag source code.
+
+    `git clone -b v2_4_x https://github.com/BZFlag-Dev/bzflag-import-3.git bzflag`
+
+2.  Go into the newly checked out source code and then the plugins directory.
+
+    `cd bzflag/plugins`
+
+3.  Create a plugin using the `newplug.sh` script.
+
+    `sh newplug.sh TeamPhoenix`
+
+4.  Delete the newly create TeamPhoenix directory.
+
+    `rm -rf TeamPhoenix`
+
+5.  Run a git clone of this repository from within the plugins directory. This should have created a new TeamPhoenix directory within the plugins directory.
+
+    `git clone https://github.com/allejo/TeamPhoenix.git`
+
+6.  Instruct the build system to generate a Makefile and then compile and install the plugin.
+
+    `cd ..; ./autogen.sh; ./configure; make; make install;`
+
+### Updating the Plugin
+
+1.  Go into the TeamPhoenix folder located in your plugins folder.
+
+2.  Pull the changes from Git.
+
+    `git pull origin master`
+
+3.  (Optional) If you have made local changes to any of the files from this project, you may receive conflict errors where you may resolve the conflicts yourself or you may simply overwrite your changes with whatever is in the repository, which is recommended. *If you have a conflict every time you update because of your local change, submit a pull request and it will be accepted, provided it's a reasonable change.*
+
+    `git reset --hard origin/master; git pull`
+
+4.  Compile the changes.
+
+    `make; make install;`
+
+Server Details
+--------------
+
+### How to Use
+
+To use this plugin after it has been compiled, simply load the plugin via the configuration file.
+
+`-loadplugin /path/to/TeamPhoenix.so,/path/to/TeamPhoenix.cfg`
+
+License
+-------
+
+[GNU General Public License Version 3.0](https://github.com/allejo/TeamPhoenix/blob/master/LICENSE.markdown)
--- TeamPhoenix.cpp
+++ TeamPhoenix.cpp
@@ -1,44 +1,19 @@
 /*
-Copyright (c) 2012 Vladimir Jimenez
-All rights reserved.
+Team Phoenix
+    Copyright (C) 2012-2014 Vladimir Jimenez
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions
-are met:
-1. Redistributions of source code must retain the above copyright
-   notice, this list of conditions and the following disclaimer.
-2. Redistributions in binary form must reproduce the above copyright
-   notice, this list of conditions and the following disclaimer in the
-   documentation and/or other materials provided with the distribution.
-3. The name of the author may not be used to endorse or promote products
-   derived from this software without specific prior written permission.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
-IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
-INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-Author:
-Vlad Jimenez (allejo)
-
-Description:
-Based on Phoenix plugin, this plugin will reset the positions for all teams
-not just one team.
-
-Slash Commands:
-N/A
-
-License:
-BSD
-
-Version:
-1.0
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "bzfsAPI.h"
@@ -46,93 +21,92 @@ Version:
 
 class TeamPhoenix : public bz_Plugin
 {
-	virtual const char* Name (){return "Team Phoenix";}
-	virtual void Init ( const char* config);
-	virtual void Event( bz_EventData *eventData );
-	virtual void Cleanup ();
-	
-	float x[256], y[256], z[256], rot[256];
-	bool newPosition[256];
+    virtual const char* Name () {return "Team Phoenix";}
+    virtual void Init (const char* config);
+    virtual void Event (bz_EventData *eventData);
+    virtual void Cleanup ();
+
+    float x[256], y[256], z[256], rot[256];
+    bool newPosition[256];
 };
 
-BZ_PLUGIN(TeamPhoenix);
+BZ_PLUGIN(TeamPhoenix)
 
-void TeamPhoenix::Init ( const char* /*commandLine*/ )
+void TeamPhoenix::Init (const char* /*commandLine*/)
 {
-	bz_debugMessage(4,"TeamPhoenix plugin loaded");
-
-    Register(bz_ePlayerDieEvent);
     Register(bz_eGetPlayerSpawnPosEvent);
-    Register(bz_ePlayerPartEvent);
+    Register(bz_ePlayerDieEvent);
     Register(bz_ePlayerJoinEvent);
+    Register(bz_ePlayerPartEvent);
 }
 
-void TeamPhoenix::Cleanup(void)
+void TeamPhoenix::Cleanup (void)
 {
-	Flush();
+    Flush();
 }
 
-void TeamPhoenix::Event(bz_EventData *eventData)
+void TeamPhoenix::Event (bz_EventData *eventData)
 {
-	switch (eventData->eventType)
-	{
-		case bz_ePlayerDieEvent:
-		{
-			bz_PlayerDieEventData_V1* dieData = (bz_PlayerDieEventData_V1*)eventData;
-			
-			if(dieData->killerID == 253)
-			{
-				newPosition[dieData->playerID] = true;
-			}
-			else
-			{
-				x[dieData->playerID] = dieData->state.pos[0];
-				y[dieData->playerID] = dieData->state.pos[1];
-				z[dieData->playerID] = dieData->state.pos[2];
-				rot[dieData->playerID] = dieData->state.rotation;
-			}
-		}
-		break;
-		
-		case bz_eGetPlayerSpawnPosEvent:
-		{
-			bz_GetPlayerSpawnPosEventData_V1* spawnData = (bz_GetPlayerSpawnPosEventData_V1*)eventData;
-			
-			if(newPosition[spawnData->playerID])
-			{
-				spawnData->handled=false;
-				newPosition[spawnData->playerID] = false;
-			}
-			else
-			{
-				spawnData->handled = true;
-				spawnData->pos[0] = x[spawnData->playerID];
-				spawnData->pos[1] = y[spawnData->playerID];
-				spawnData->pos[2] = z[spawnData->playerID];
-				spawnData->rot = rot[spawnData->playerID];
-			}
-		}
-		break;
-		
-		case bz_ePlayerPartEvent:
-		{
-			bz_PlayerJoinPartEventData_V1* partData = (bz_PlayerJoinPartEventData_V1*)eventData;
-			
-			x[partData->playerID] = '\0';
-			y[partData->playerID] = '\0';
-			z[partData->playerID] = '\0';
-			rot[partData->playerID] = '\0';
-		}
-		break;
-		
-		case bz_ePlayerJoinEvent:
-		{
-			bz_PlayerJoinPartEventData_V1* joinData = (bz_PlayerJoinPartEventData_V1*)eventData;
-			
-			newPosition[joinData->playerID] = true;
-		}
-		break;
-		
-		default:break;
-	}
+    switch (eventData->eventType)
+    {
+        case bz_eGetPlayerSpawnPosEvent:
+        {
+            bz_GetPlayerSpawnPosEventData_V1* spawnData = (bz_GetPlayerSpawnPosEventData_V1*)eventData;
+
+            if (newPosition[spawnData->playerID])
+            {
+                spawnData->handled = false;
+                newPosition[spawnData->playerID] = false;
+            }
+            else
+            {
+                spawnData->handled = true;
+                spawnData->pos[0]  = x[spawnData->playerID];
+                spawnData->pos[1]  = y[spawnData->playerID];
+                spawnData->pos[2]  = z[spawnData->playerID];
+                spawnData->rot     = rot[spawnData->playerID];
+            }
+        }
+        break;
+
+        case bz_ePlayerDieEvent:
+        {
+            bz_PlayerDieEventData_V1* dieData = (bz_PlayerDieEventData_V1*)eventData;
+
+            if (dieData->killerID == 253)
+            {
+                newPosition[dieData->playerID] = true;
+            }
+            else
+            {
+                x[dieData->playerID]   = dieData->state.pos[0];
+                y[dieData->playerID]   = dieData->state.pos[1];
+                z[dieData->playerID]   = dieData->state.pos[2];
+                rot[dieData->playerID] = dieData->state.rotation;
+            }
+        }
+        break;
+
+        case bz_ePlayerJoinEvent:
+        {
+            bz_PlayerJoinPartEventData_V1* joinData = (bz_PlayerJoinPartEventData_V1*)eventData;
+
+            newPosition[joinData->playerID] = true;
+        }
+        break;
+
+        case bz_ePlayerPartEvent:
+        {
+            bz_PlayerJoinPartEventData_V1* partData = (bz_PlayerJoinPartEventData_V1*)eventData;
+
+            x[partData->playerID]   = '\0';
+            y[partData->playerID]   = '\0';
+            z[partData->playerID]   = '\0';
+            rot[partData->playerID] = '\0';
+        }
+        break;
+
+        default:
+            break;
+    }
 }
